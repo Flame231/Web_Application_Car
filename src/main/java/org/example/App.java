628ea43f0b71d5f425ca4c7f74f47c3eb97596a8
System Administrator <flame5930@gmail.com>
2026-04-10 15:20:25 +0300

    создание структуры веб-приложения

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,37 @@
 package org.example;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends HttpServlet
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       doGet(request, response);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String total = request.getParameter("total");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World say Hello!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World say Hello!</h1>");
+        out.println(total);
+
+
+        out.println("<a href=\"index.jsp\">return</a>");
+        out.println("</body>");
+        out.println("</html>");
+
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: System Administrator
+  Date: 10.04.2026
+  Time: 14:18:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <h1>hello!</h1>
+    <form action="sayhello" method="post">
+        total:<input name="total" type="text"/>
+        <input type="submit">
+    </form>
+</head>
+<body>
+
+</body>
+</html>
